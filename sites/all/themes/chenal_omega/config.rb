ea4f2678fdf37e8657cbcbc4cc4f5cec3ac6200e
jrcallicott <jrcallicott@192.168.11.105>
2013-11-16 11:51:15 -0600

    Updating config

--- sites/all/themes/chenal_omega/config.rb
+++ sites/all/themes/chenal_omega/config.rb
@@ -1,51 +1,30 @@
-##
-## This file is only needed for Compass/Sass integration. If you are not using
-## Compass, you may safely ignore or delete this file.
-##
-## If you'd like to learn more about Sass and Compass, see the sass/README.txt
-## file for more information.
-##
-
-# Default to development if environment is not set.
-saved = environment
-if (environment.nil?)
-  environment = :development
-else
-  environment = saved
-end
-
-# Location of the theme's resources.
-css_dir = "css"
-sass_dir = "sass"
-images_dir = "images"
-generated_images_dir = images_dir + "/generated"
-javascripts_dir = "js"
-
-# Require any additional compass plugins installed on your system.
+# Require any additional compass plugins here.
 require 'compass-normalize'
 require 'rgbapng'
 require 'toolkit'
 require 'susy'
 require 'sass-globbing'
 
-##
-## You probably don't need to edit anything below this.
-##
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "js"
+fonts_dir = "fonts"
 
-# You can select your preferred output style here (:expanded, :nested, :compact
-# or :compressed).
-output_style = (environment == :production) ? :expanded : :nested
+output_style = :nested
 
-# To enable relative paths to assets via compass helper functions. Since Drupal
-# themes can be installed in multiple locations, we don't need to worry about
-# the absolute path to the theme from the server omega.
-relative_assets = true
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
 
-# Conditionally enable line comments when in development mode.
-line_comments = (environment == :production) ? false : true
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+color_output = false
 
-# Output debugging info in development mode.
-sass_options = (environment == :production) ? {} : {:debug_info => true}
 
-# Add the 'sass' directory itself as an import path to ease imports.
-add_import_path 'sass'
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
