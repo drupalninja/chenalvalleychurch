032362c9e1014fdd985a53a5f47084c29ecf1470
Jay Callicott <jaykali@42248.no-reply.drupal.org>
2016-02-15 19:58:26 -0600

    Updated body

---
+++ sites/all/themes/chenal_omega/templates/views-view-field--openchurch-latest-blogs--block-2--body.tpl.php
@@ -0,0 +1,2 @@
+<img style="float: left; margin: 0 15px 15px 0; border: 3px solid #ccc;" src="/sites/chenalvalleychurch.org/files/styles/oc_staff_image/public/Bert-web.jpg" alt="Bert Reynolds">
+<?php print $output; ?>
