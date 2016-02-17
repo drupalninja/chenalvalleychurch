ebd0e8f8b80130fd9e0e0dce79082f323cabdfdc
Jay Callicott <jaykali@42248.no-reply.drupal.org>
2016-02-16 20:41:50 -0600

    Updated blog news

--- sites/all/themes/chenal_omega/templates/views-view-field--openchurch-latest-blogs--page-1--body.tpl.php
+++ sites/all/themes/chenal_omega/templates/views-view-field--openchurch-latest-blogs--page-1--body.tpl.php
@@ -1,4 +1,3 @@
-<img style="float: left; margin: 0 15px 15px 0; border: 3px solid #ccc;" src="/sites/chenalvalleychurch.org/files/styles/oc_staff_image/public/Bert-web.jpg" alt="Bert Reynolds">
 <?php if (!empty($row->field_field_audio_embed[0]['raw']['value'])): ?>
   <div style="float: right; margin: 0 2px 15px 15px;">
   <?php
@@ -7,3 +6,4 @@
   </div>
 <?php endif; ?>
 <?php print $output; ?>
+
